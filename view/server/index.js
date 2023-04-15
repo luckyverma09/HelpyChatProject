7d67085adaa38581b1159296f579eedf312a0aca
CodeUpSubodh <94781836+CodeUpSubodh@users.noreply.github.com>
2023-04-15 11:03:03 +0530

    Add files via upload

--- view/server/index.js
+++ view/server/index.js
@@ -1,7 +1,48 @@
-const express=require("express");
-const app=express();
-app.use(express.json);
+const express = require("express");
+const OPENAI_API_KEY = "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";
+const { Configuration, OpenAIApi } = require("openai");
+const cors = require("cors");
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-app.listen(6000,() =>{
-    console.log("Server is listening on port 3000")
-})
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.post("/chat", (req, res) => {
+  const question = req.body.question;
+
+  openai
+    .createCompletion({
+      model: "text-davinci-003",
+      prompt: question,
+      max_tokens: 4000,
+      temperature: 0,
+    })
+    .then((response) => {
+      return response.data.choices?.[0].text;
+    })
+    .then((answer) => {
+      console.log({ answer });
+      const array = answer
+        ?.split("\n")
+        .filter((value) => value)
+        .map((value) => value.trim());
+
+      return array;
+    })
+    .then((answer) => {
+      res.json({
+        answer: answer,
+        propt: question,
+      });
+    });
+  console.log({ question });
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
