5ae6ac072fabd5c53cf2e8fa65479f240933f763
CodeUpSubodh <94781836+CodeUpSubodh@users.noreply.github.com>
2023-04-19 11:43:54 +0530

    first commit

--- view/src/App.js
+++ view/src/App.js
@@ -5,16 +5,18 @@ import About from "./Componenets/SubComponents/About";
 import Contact from "./Componenets/SubComponents/Contact";
 import Signup from "./Componenets/SubComponents/Signup";
 import Login from "./Componenets/SubComponents/Login";
+import FrontPage from "./Componenets/SubComponents/FrontPage";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<FrontPage/>} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/Signup" element={<Signup />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/home" element={<Home/>}/>
       </Routes>
     </>
   );
